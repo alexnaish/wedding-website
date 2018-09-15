91f5362ecbe94904467ef2f54991e5ebadb8c1f9
Alex Naish <alex@naish.io>
2018-09-15 22:25:29 +0100

    Split out API files

---
+++ api/submission.js
@@ -0,0 +1,10 @@
+module.exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://alexandsarawedding.co.uk'
+    },
+    body: JSON.stringify({ data: 'Submission!' })
+  };
+};
+
---
+++ api/validate.js
@@ -0,0 +1,9 @@
+module.exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://alexandsarawedding.co.uk'
+    },
+    body: JSON.stringify({ data: 'Validate!' })
+  };
+};
