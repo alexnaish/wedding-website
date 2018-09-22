552f68eda9d83ac0062501bcaa6b1810e5f1e3b1
Alex Naish <alex@naish.io>
2018-09-22 19:38:30 +0100

    Fix dynasty issue

--- api/services/guest/index.js
+++ api/services/guest/index.js
@@ -1,9 +1,7 @@
 // const Guest = require('./model');
 
 const dynasty = require('dynasty')({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'eu-west-1'
+  region: 'eu-west-2'
 });
 
 const Guest = dynasty.table('wedding-guests');
