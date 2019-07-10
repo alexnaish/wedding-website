f42f403c37ff1dc9960fffbdc55fddfcb156636d
Alex Naish <alex@naish.io>
2019-07-10 21:39:23 +0100

    fix: change to mailgun instead of gmail

--- backend/api/callback/index.js
+++ backend/api/callback/index.js
@@ -1,29 +1,19 @@
-const nodemailer = require('nodemailer');
+const mailgun = require("mailgun-js");
 const app = require('../../util/server');
 
 app.post('*', async (req, res) => {
-
   try {
 
-		let transporter = nodemailer.createTransport({
-			host: 'smtp.googlemail.com',
-			port: 465,
-			secure: true,
-			auth: {
-					user: process.env.EMAIL_ACCOUNT,
-					pass: process.env.EMAIL_PASSWORD
-			}
-		});
-
-		let mailOptions = {
-				from: `"Wedding Website" <${process.env.EMAIL_ACCOUNT}>`,
-				to: process.env.EMAIL_ACCOUNT,
-				subject: 'Change Notification',
-				html: `An RSVP record has changed. <p>${JSON.stringify(req.body)}</p>`
+		const mg = mailgun({apiKey: process.env.EMAIL_API_KEY, domain: process.env.EMAIL_DOMAIN });
+		const data = {
+			from: 'Wedding Website <lexi.drage@gmail.com>',
+			to: 'lexi.drage@gmail.com, sz.sacka@gmail.com',
+			subject: 'RSVP Change Notification',
+			html: `An RSVP record has changed. <p>${JSON.stringify(req.body)}</p>`
 		};
+		await mg.messages().send(data);
+		res.status(200).json({});
 
-		await transporter.sendMail(mailOptions);
-    res.status(200).json({});
   } catch (error) {
     console.error('callback', error);
     res.status(500).json({
