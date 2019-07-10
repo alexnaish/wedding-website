fa5e433783b879d118cdc6b6183f6b656a39737f
Alex Naish <alex@naish.io>
2019-07-10 21:55:37 +0100

    feat: make notification friendlier

--- backend/api/callback/index.js
+++ backend/api/callback/index.js
@@ -3,13 +3,17 @@ const app = require('../../util/server');
 
 app.post('*', async (req, res) => {
   try {
-
+		const { name, attendance, diet } = req.body;
 		const mg = mailgun({apiKey: process.env.EMAIL_API_KEY, domain: process.env.EMAIL_DOMAIN });
 		const data = {
 			from: 'Wedding Website <changes@alexandsarawedding.co.uk>',
 			to: process.env.EMAIL_RECIPIENTS,
 			subject: 'RSVP Change Notification',
-			html: `An RSVP record has changed. <p>${JSON.stringify(req.body)}</p>`
+			html: `An RSVP record has changed:
+			<p>Name: ${name}</p>
+			<p>Attending: ${attendance ? 'Yes' : 'No'}</p>
+			<p>Dietary notes: ${diet}</p>
+			`
 		};
 		await mg.messages().send(data);
 		res.status(200).json({});
