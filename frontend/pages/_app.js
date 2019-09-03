73915528766e0b2037c5b286f94b714e2cbd6cfa
Alex Naish <alex@naish.io>
2019-09-03 18:41:58 +0100

    feat: attempt to support ie11

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 import * as Sentry from '@sentry/browser';
 
 import Application from '../components/App';
@@ -40,6 +41,13 @@ class MyApp extends App {
 
     return (
       <Application>
+				<Head>
+					<meta charSet="utf-8" />
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<title>Alex and Sara Get Married!</title>
+					<meta name="description" content="The website for information of and to RSVP for Alex and Sara's wedding." />
+					<script src="https://polyfill.io/v3/polyfill.min.js?flags=gated&rum=true&features=Object.assign%2CArray.from%2Cdefault%2Ces6%2CArray.prototype.includes%2CPromise%2CMap%2CSet"></script>
+				</Head>
         <Container>
 					<CountryContext.Provider value={countryCode}>
 						<Navigation />
