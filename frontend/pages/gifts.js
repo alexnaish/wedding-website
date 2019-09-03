750f572d4bdcc831f253e41db7dff6cacecd93fd
Alex Naish <alex@naish.io>
2019-09-03 09:41:08 +0100

    fix: add keys to poem lines

--- frontend/pages/gifts.js
+++ frontend/pages/gifts.js
@@ -7,7 +7,7 @@ import Internationalised from '../components/Internationalised';
 export default () => {
 	return (
 		<Section heading={<Internationalised word="gifts" />} textAlign="center">
-			<Internationalised word="poem" transform={val => val.map(line => line ? <p>{line}</p> : <br />)} />
+			<Internationalised word="poem" transform={val => val.map((line, index) => line ? <p key={index}>{line}</p> : <br key={index} />)} />
 			<Divider theme="dark" narrow={true} />
 			<p>Just remember, what means the most</p>
 			<p>Is that you're here with us to a raise a toast!</p>
