383acef91fd73d5141832699439edc6c463d131c
Alex Naish <alex@naish.io>
2020-08-05 15:39:41 +0100

    fix: remove the en from the hungarian date

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,5 +4,5 @@ import Section from '../components/Section';
 export default () => <div>
   <Splash />
   <Section heading="England - 6th March 2021" intro="Welcome to our wedding website!" />
-  <Section heading="Magyarország - 2021 Április 17-én." intro="Köszöntünk az esküvői weboldalunkon!" />
+  <Section heading="Magyarország - 2021 Április 17" intro="Köszöntünk az esküvői weboldalunkon!" />
 </div>;
